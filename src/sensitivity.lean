adfaf61df10288f355e035627d77b863625119a2
Reid Barton <rwbarton@gmail.com>
2019-08-02 11:33:33 -0400

    Definition of V and f.

---
+++ src/sensitivity.lean
@@ -0,0 +1,56 @@
+import data.real.basic
+import linear_algebra.dimension
+
+noncomputable theory
+
+/-- The free vector space on vertices of a hypercube, defined inductively. -/
+def V : ℕ → Type
+| 0 := ℝ
+| (n+1) := V n × V n
+
+instance : Π n, add_comm_group (V n) :=
+begin
+  apply nat.rec,
+  { dunfold V, apply_instance },
+  { introsI n IH, dunfold V, apply_instance }
+end
+
+instance : Π n, vector_space ℝ (V n) :=
+begin
+  apply nat.rec,
+  { dunfold V, apply_instance },
+  { introsI n IH, dunfold V, apply_instance }
+end
+
+lemma dim_V {n : ℕ} : vector_space.dim ℝ (V n) = 2^n :=
+begin
+  induction n with n IH,
+  { apply dim_of_field },
+  { dunfold V,
+    rw [dim_prod, IH, pow_succ, two_mul] }
+end
+
+/-- The linear operator f_n corresponding to Huang's matrix A_n. -/
+noncomputable def f : Π n, V n →ₗ[ℝ] V n
+| 0 := 0
+| (n+1) :=
+  linear_map.pair
+    (linear_map.copair (f n) linear_map.id)
+    (linear_map.copair linear_map.id (-f n))
+
+lemma f_squared {n : ℕ} : ∀ v, (f n) (f n v) = (n : ℝ) • v :=
+-- The (n : ℝ) is necessary since `n • v` refers to the multiplication defined
+-- using only the addition of V.
+begin
+  induction n with n IH,
+  { intro v, dunfold f, simp, refl },
+  { rintro ⟨v, v'⟩,
+    ext,
+    { dunfold f V,
+      conv_rhs { change ((n : ℝ) + 1) • v, rw add_smul },
+      simp [IH] },
+    { dunfold f V,
+      conv_rhs { change ((n : ℝ) + 1) • v', rw add_smul },
+      have : Π (x y : V n), -x + (y + x) = y := by { intros, abel }, -- ugh
+      simp [IH, this] } }
+end
