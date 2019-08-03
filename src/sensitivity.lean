27e62f031d2347e1d2d8b3e56a7f840b2e45e043
Patrick Massot <patrickmassot@free.fr>
2019-08-03 19:52:21 +0200

    Simplify the prood of f_image_g

--- src/sensitivity.lean
+++ src/sensitivity.lean
@@ -269,95 +269,16 @@ begin
   rw[g], intros x₁ x₂ H_eq, simp at *, exact H_eq.right
 end
 
-@[simp] lemma f_0 {x} : (f 0).to_fun x = 0 := rfl
-
-@[simp]lemma f_neg {m} {x} : (f m).to_fun x + (-f m).to_fun x = 0 := by change (f m) x + (-f m) x = 0; simp
-
-lemma cast_lemma_1 {m : ℕ} : 0 ≤ (1 + (nat.cast m) : ℝ) :=
-by {change (0 : ℝ) ≤ (1 + ↑m : ℝ), suffices this : 0 ≤ (↑m : ℝ), by {linarith}, simp}
-
-lemma cast_lemma_2 {m : ℕ} : (nat.cast (nat.succ m) : ℝ) = (1 + nat.cast m : ℝ) :=
-by change ↑(nat.succ m) = (1 + ↑m : ℝ); simp
-
-lemma f_image_g_aux₅ (m : ℕ) (v₁ v₂ : V m) (ih : ∀ (w_snd : V (m + 0)), (1 + nat.cast m : ℝ) • w_snd = w_snd + (f m).to_fun ((f m).to_fun w_snd)) :
- (1 + nat.cast (nat.succ m) : ℝ) • v₁ =
-    v₁ +
-      ((linear_map.pair (linear_map.copair (f m) linear_map.id) (linear_map.copair linear_map.id (-f m))).to_fun
-         ((linear_map.pair (linear_map.copair (f m) linear_map.id) (linear_map.copair linear_map.id (-f m))).to_fun
-            (v₁, v₂))).fst :=
-begin
-  change _ = v₁ + (_ + _), /- `tidy` says -/ dsimp at *, simp at *, simp at *, unfold_coes, 
-  change _ = v₁ + ((v₁ + (-f m).to_fun (v₂)) + ((f m).to_fun ((f m).to_fun v₁ + v₂))),
-  specialize ih v₁, rw[add_smul],
-  have this : ((nat.cast (nat.succ m) : ℝ) • v₁) = (1 + nat.cast m : ℝ) • v₁,
-    by { rw[cast_lemma_2] }, rw[this],
-  rw[ih], simp[(f m).add],
-  ac_change _ = ((f m).to_fun v₂ + (-f m).to_fun v₂) + ((f m).to_fun ((f m).to_fun v₁)),
-  simp
-end
-
-lemma f_image_g_aux₄ (m : ℕ) (v₁ v₂ : V m) (ih : ∀ (w_snd : V (m + 0)), (1 + nat.cast m : ℝ) • w_snd = w_snd + (f m).to_fun ((f m).to_fun w_snd)) :
-  (1 + nat.cast (nat.succ m) : ℝ) • v₂ =
-    v₂ +
-      ((linear_map.pair (linear_map.copair (f m) linear_map.id) (linear_map.copair linear_map.id (-f m))).to_fun
-         ((linear_map.pair (linear_map.copair (f m) linear_map.id) (linear_map.copair linear_map.id (-f m))).to_fun
-            (v₁, v₂))).snd :=
-begin
-  change _ = v₂ + (_ + _), /- `tidy` says -/ dsimp at *, unfold_coes, 
-  change _ = v₂ + (((f m).to_fun v₁ + v₂) + ((-f m).to_fun (v₁ + (-f m).to_fun v₂))),
-  specialize ih v₂, rw[add_smul],
-  have this : ((nat.cast (nat.succ m) : ℝ) • v₂) = (1 + nat.cast m : ℝ) • v₂,
-    by { rw[cast_lemma_2] }, rw[this],
-  rw[ih], simp[(- f m).add],
-  ac_change _ = ((f m).to_fun v₁ + (-f m).to_fun v₁) + (-f m).to_fun ((-f m).to_fun v₂),
-  simp,
-  have this₂ : ∀ {x}, (-f m).to_fun x = (-1 : ℝ) • ((f m).to_fun x),
-    by {/- `tidy` says -/ intros x, simp at *, refl},
-  rw this₂, rw this₂, rw[(f m).smul], simp
-end
-
-lemma f_image_g_aux₃ (m : ℕ) (w_snd : V (m + 0)) : (1 + nat.cast m : ℝ) • w_snd = w_snd + (f m).to_fun ((f m).to_fun w_snd) :=
-begin
-  induction m with m ih,
-    {tidy, change (1 + 0 : ℝ) * _ = _, simp},
-    {rw[f], cases w_snd with v₁ v₂,
-    /- `tidy` says -/ dsimp at *, simp at *, tactic.ext1 [] {new_goals := tactic.new_goals.all},
-    work_on_goal 0 { dsimp at *, simp at *, simp at * },
-    work_on_goal 1 { dsimp at *, simp at *, simp at * },
-      { apply f_image_g_aux₅, exact ih },
-      { apply f_image_g_aux₄, exact ih },
-      }
-end
-
-lemma f_image_g_aux₁ (m : ℕ) (w_fst w_snd : V (m + 0)) (hv_w : V m) (h_1 : w_fst = (f m) w_snd + real.sqrt (1 + ↑m) • w_snd)
-: ((f (m + 1)).to_fun (w_fst, w_snd)).fst = real.sqrt (1 + nat.cast m) • w_fst :=
-begin
-  subst h_1, simp at *, unfold_coes,
-  rw[f], change (linear_map.copair (f m) linear_map.id).to_fun _ = _,
-  change _ + _ = _, /- `tidy` says -/ dsimp at *, simp at *, unfold_coes,
-  suffices this : (1 + nat.cast m : ℝ) • w_snd = w_snd + (f m).to_fun ((f m).to_fun w_snd),
-    by { rw[smul_add, smul_smul],
-         simp[show real.sqrt (1 + nat.cast m) * real.sqrt (1 + nat.cast m) = (1 + nat.cast m : ℝ),
-              by rw[real.mul_self_sqrt cast_lemma_1], this]},
-  apply f_image_g_aux₃
-end
-
-lemma f_image_g_aux₂ (m : ℕ) (w_fst w_snd : V (m + 0)) (h_1 : w_fst = (f m) w_snd + real.sqrt (1 + ↑m) • w_snd)
-  : ((f (m + 1)).to_fun (w_fst, w_snd)).snd = real.sqrt (1 + nat.cast m) • w_snd :=
-by {rw[f], change (linear_map.copair _ _).to_fun _ = _, change _ + _ = _, subst h_1, /- `tidy` says -/ dsimp at *, simp at *, refl}
-
 -- I don't understand why the type ascription is necessary here, when f_squared worked fine
 lemma f_image_g {m : ℕ} (w : V (m + 1)) (hv : ∃ v, w = g m v) :
   (f (m + 1) : V (m + 1) → V (m + 1)) w = real.sqrt (m + 1) • w :=
-begin -- tidy, take the wheel!
-  /- `tidy` says -/ cases hv, cases w, dsimp at *, simp at *,
-  tactic.ext1 [] {new_goals := tactic.new_goals.all}, work_on_goal 0
-  { dsimp at *, injections_and_clear, dsimp at *, induction h_2,
-    simp at *, unfold_coes }, work_on_goal 1
-    { dsimp at *, injections_and_clear, dsimp at *,
-      induction h_2, simp at *, unfold_coes },
-  { apply f_image_g_aux₁; from ‹_› },
-  { apply f_image_g_aux₂; from ‹_› }
+begin
+  rcases hv with ⟨v, rfl⟩,
+  dsimp [g],
+  erw f,
+  simp [f_squared],
+  rw [smul_add, smul_smul, real.mul_self_sqrt (by exact_mod_cast zero_le _ : 0 ≤ (1 : ℝ) + m), add_smul, one_smul], 
+  abel,
 end
 
 variables {m : ℕ} (H : set (Q (m + 1))) (hH : fintype.card H ≥ 2^m + 1)
